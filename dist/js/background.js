8fabdc15a25b78ce5380dd255f4e217218324337
Reinford0 <56060259+Reinford0@users.noreply.github.com>
2019-10-04 11:51:14 +0800

    Update background.js

--- dist/js/background.js
+++ dist/js/background.js
@@ -46,7 +46,73 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
                 url: msg.fileUrl,
                 thisIndex: msg.thisIndex,
                 tabid: sender.tab.id
-            };            };                tabid: sender.tab.id
+            };            };                tabid: sender.tab.id"use strict";
+// 设置 referer
+chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+    let setReferer = false;
+    for (let i = 0; i < details.requestHeaders.length; ++i) {
+        if (details.requestHeaders[i].name === 'Referer') {
+            setReferer = true;
+            details.requestHeaders[i].value = 'https://www.pixiv.net';
+            break;
+        }
+    }
+    if (!setReferer) {
+        details.requestHeaders.push({
+            name: 'Referer',
+            value: 'https://www.pixiv.net'
+        });
+    }
+    return {
+        requestHeaders: details.requestHeaders
+    };
+}, {
+    urls: ['*://*.pixiv.net/*', '*://*.pximg.net/*']
+}, ['blocking', 'requestHeaders']);
+// 当点击扩展图标时，切换显示/隐藏下载面板
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // 打开下载面板
+    chrome.tabs.sendMessage(tab.id, {
+        msg: 'click_icon'
+    });
+});
+// 因为下载完成的顺序和发送顺序可能不一致，所以需要存储任务的数据
+let donwloadListData = {};
+// 接收下载请求
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.msg === 'send_download') {
+        // 开始下载
+        chrome.downloads.download({
+            url: msg.fileUrl,
+            filename: msg.fileName,
+            conflictAction: 'overwrite',
+            saveAs: false
+        }, id => {
+            // 成功建立下载任务时，返回下载项的 id
+            chrome.tabs.sendMessage(sender.tab.id, { msg: 'id', id });
+            donwloadListData[id] = {
+                no: msg.no,
+                url: msg.fileUrl,
+                thisIndex: msg.thisIndex,
+                tabid: sender.tab.id
+            };
+        });
+    }
+});
+// 监听下载事件
+chrome.downloads.onChanged.addListener(function (detail) {
+    // 下载完成后
+    if (detail.state !== undefined && detail.state.current === 'complete') {
+        // 根据 detail.id 取出保存的信息
+        const msg = 'downloaded';
+        const data = donwloadListData[detail.id];
+        const id = detail.id;
+        if (!data)
+            return;
+        chrome.tabs.sendMessage(data.tabid, { msg, data, id });
+    }
+});
+
 
 
         });
