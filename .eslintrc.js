d0f88f9c8ac112f962a416d29b5968189ef49e7b
zin <cnzzing@gmail.com>
2019-11-15 16:59:55 +0800

    修改 prettier 配置 增加 prettier 相关文件忽略 移除 globals 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,15 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   env: {
-    node: true,
     browser: true
   },
-  extends: ['plugin:@typescript-eslint/eslint-recommended', 'prettier/@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier/@typescript-eslint',
+    'prettier'
+  ],
   rules: {
     'no-console': 'off',
     'no-useless-catch': 'off'
-  },
-  globals: {
-    fetch: true,
-    Blob: true,
-    Image: true,
-    XMLHttpRequest: true,
-    chrome: true,
-    zip: true,
-    GIF: true,
-    Whammy: true,
-    Viewer: true,
-    xzLang: true
   }
 }
