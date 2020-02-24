3b13156d53adc67c718993381f845b72c7e423eb
炼铜术士 <32679716+jiaer24@users.noreply.github.com>
2020-02-24 17:31:09 +0800

    Update langText.ts
    优化日语翻译

--- src/ts/modules/langText.ts
+++ src/ts/modules/langText.ts
@@ -30,7 +30,7 @@ const langText = {
   ],
   _多p下载前几张: [
     '设置作品张数',
-    '作品ごとにダウンロードされた画像の数',
+    '作品ごとにダウンロード画像の数',
     'Number of images downloaded per work',
     '設定作品張數'
   ],
@@ -42,19 +42,19 @@ const langText = {
   ],
   _不能含有tag: [
     '不能含有 tag&nbsp;',
-    '指定した tagを除外する',
+    '指定した tag を除外する',
     'Exclude specified tag',
     '不能含有 tag&nbsp;'
   ],
   _排除tag的提示文字: [
     '您可在下载前设置要排除的tag，这样在下载时将不会下载含有这些tag的作品。不区分大小写；如需排除多个tag，请使用英文逗号分隔。请注意要排除的tag的优先级大于要包含的tag的优先级。',
-    "ダウンロードする前に、除外する tagを設定できます。大文字と小文字を区別しない；複数の tagを設定する必要がある場合は、','で区切ってください。除外された tagは、含まれている tagよりも優先されます",
+    "ダウンロード前に、除外する tag を設定できます。大文字と小文字を区別しない；複数の tag を設定する必要がある場合は、「,」で区切ってください。除外された tag は、必要な tag よりも優先されます",
     "Before downloading, you can set the tag you want to exclude. Not case sensitive; If you need to set multiple tags, you can use ',' separated. The excluded tag takes precedence over the included tag",
     '您可在下載前設定要排除的tag，這樣在下載時將不會下載含有這些tag的作品。不區分大小寫；如需排除多個tag，請使用英文逗號分隔。請注意要排除的tag的優先等級大於要包含的tag的優先等級。'
   ],
   _设置了排除tag之后的提示: [
     '排除 tag：',
-    ' tagを除外：',
+    '以下の tag を除外：',
     'Excludes tag: ',
     '排除 tag：'
   ],
@@ -66,13 +66,13 @@ const langText = {
   ],
   _必须tag的提示文字: [
     '您可在下载前设置作品里必须包含的tag，不区分大小写；如需包含多个tag，请使用英文逗号分隔。',
-    "ダウンロードする前に、必要な tagを設定することができます。大文字と小文字を区別しない；複数の tagを設定する必要がある場合は、','で区切ってください。",
+    "ダウンロードする前に、必要な tag を設定することができます。大文字と小文字を区別しない；複数の tag を設定する必要がある場合は、「,」で区切ってください。",
     "Before downloading, you can set the tag that must be included. Not case sensitive; If you need to set multiple tags, you can use ',' separated. ",
     '您可在下載前設定作品裡必須包含的tag，不區分大小寫；如需包含多個tag，請使用英文逗號分隔。'
   ],
   _设置了必须tag之后的提示: [
     '包含 tag：',
-    ' tagを含める：',
+    '以下の tag を含める：',
     'Include tag: ',
     '包含 tag：'
   ],
@@ -205,7 +205,7 @@ const langText = {
   ],
   _checkWantPageRule1Arg3: [
     '从本页开始下载-num-个作品',
-    'このページから-num-枚の作品をダウンロード。',
+    'このページから -num- 枚の作品をダウンロード。',
     'Download -num- works from this page.',
     '從本頁開始下載-num-個作品'
   ],
@@ -223,7 +223,7 @@ const langText = {
   ],
   _checkWantPageRule1Arg6: [
     '从本页开始下载-num-页',
-    '現在のページから-num-ページをウンロードします',
+    '現在のページから -num- ページをウンロードします',
     'download -num- pages from the current page',
     '從本頁開始下載-num-頁'
   ],
@@ -253,13 +253,13 @@ const langText = {
   ],
   _checkWantPageRule1Arg12: [
     '下载排行榜前 -num- 个作品',
-    'キング前 -num- 位の作品をダウンロードする。',
+    'ランク前 -num- 位の作品をダウンロードする。',
     'download the top -num- works in the ranking list',
     '下載排行榜前 -num- 個作品'
   ],
   _请输入最低收藏数和要抓取的页数: [
     '请输入最低收藏数和要抓取的页数，用英文逗号分开。\n类似于下面的形式: \n1000,1000',
-    "ボックマークの最小数とクロールするページ数を，','で区切って入力してください。\n例えば：\n1000,1000",
+    "ボックマークの最小数とクロールするページ数を，「,」で区切って入力してください。\n例えば：\n1000,1000",
     "Please type the minimum number of bookmarks, and the number of pages to be crawled, separated by ','.\nE.g:\n1000,1000",
     '請輸入最低收藏數和要擷取的頁數，用英文逗號分開。\n類似於下面的形式: \n1000,1000'
   ],
@@ -277,7 +277,7 @@ const langText = {
   ],
   _任务开始1: [
     '从本页开始下载{}页',
-    'このページから{}ページをダウンロードする',
+    'このページから {} ページをダウンロードする',
     'download {} pages from this page',
     '從本頁開始下載{}頁'
   ],
@@ -320,12 +320,12 @@ const langText = {
   ],
   _多图建立目录提示: [
     '当你下载多图作品时，下载器可以使用作品 id 自动创建一个目录，保存里面的图片。',
-    'マルチイメージをダウンロードする時、自動的に作品idを使ってフォルダを作成し、中のイラストを保存することができます。',
+    'マルチイメージをダウンロードする時、自動的に作品idを使ってフォルダを作成し、イメージをその中で保存することができます。',
     'When you download a multi-image work, the downloader can automatically create a directory with the work id and save the images inside.',
     '當你下載多圖作品時，下載器可以使用作品 id 自動創建一個目錄，保存裏面的圖片。'
   ],
   _不下载: ['不下载', 'ダウンロードしない', 'Do not download', '不下載'],
-  _全部下载: ['全部下载', '全部ダウンロードする', 'Download all', '全部下載'],
+  _全部下载: ['全部下载', '全部ダウンロード', 'Download all', '全部下載'],
   _下载前几张图片: [
     '下载前几张图片：',
     '最初のいくつかの画像：',
@@ -334,7 +334,7 @@ const langText = {
   ],
   _不下载多图作品: [
     '不下载多图作品',
-    'マルチイメージ作品をダウンロードしないでください',
+    'マルチイメージ作品をダウンロードしない',
     'Do not download multi-image works',
     '不下載多圖作品'
   ],
@@ -364,13 +364,13 @@ const langText = {
   _zipFile: ['Zip 文件', 'ZIP ファイル', 'Zip file', 'Zip 檔案'],
   _当前作品个数: [
     '当前有 {} 个作品 ',
-    '今は{}枚の作品があります ',
+    '今は　{}　枚の作品があります ',
     'There are now {} works ',
     '目前有 {} 個作品 '
   ],
   _排行榜进度: [
     '已抓取本页面第{}部分',
-    'このページの第{}部がクロールされました',
+    'このページの第　{}　部がクロールされました',
     'Part {} of this page has been crawled',
     '已擷取本頁面第{}部分'
   ],
@@ -382,37 +382,37 @@ const langText = {
   ],
   _抓取多少个作品: [
     '抓取本页面 {} 个作品',
-    'このページの「」つの作品をクロールします',
+    'このページの {} つの作品をクロールします',
     'Crawl this page {} works',
     '擷取本頁面 {} 個作品'
   ],
   _相关作品抓取完毕: [
     '相关作品抓取完毕。包含有{}个作品，开始获取作品信息。',
-    '関連作品はクロールされました。 {}作品を含み、その作品に関する情報の取得を開始します。',
+    '関連作品はクロールされました。 {} 作品を含み、その作品に関する情報の取得を開始します。',
     'The related works have been crawled. Contains {} works and starts getting information about the work.',
     '相關作品擷取完畢。包含有{}個作品，開始取得作品資訊。'
   ],
   _排行榜任务完成: [
     '本页面抓取完毕。<br>当前有{}个作品，开始获取作品信息。',
-    'このページはクロールされ。<br>{}枚の作品があります。 作品情報の取得を開始します。',
+    'このページのクロール終了。<br>{}枚の作品があります。 作品情報の取得を開始します。',
     'This page is crawled and now has {} works.<br> Start getting the works for more information.',
     '本頁面擷取完畢。<br>目前有{}個作品，開始取得作品資訊。'
   ],
   _列表页抓取进度: [
     '已抓取列表页{}个页面',
-    '{}のリストページを取得しました',
+    '{} のリストページを取得しました',
     'Has acquired {} list pages',
     '已擷取清單頁{}個頁面'
   ],
   _搜索页已抓取所有页面: [
     '已抓取本 tag 的所有页面，开始获取图片网址',
-    '現在 tagの全ページを取得している、画像URLの取得が開始されます',
+    '現在 tag の全ページを取得している、画像 URL の取得が開始されます',
     'Gets all pages of the current tag, starts to get the image URL',
     '已擷取本 tag 的所有頁面，開始取得圖片網址'
   ],
   _列表页抓取完成: [
     '列表页面抓取完成，开始获取图片网址',
-    'リストページがクロールされ、画像URLの取得が開始されます',
+    'リストページがクロールされ、画像 URL の取得が開始されます',
     'The list page is crawled and starts to get the image URL',
     '清單頁面擷取完成，開始取得圖片網址'
   ],
@@ -430,13 +430,13 @@ const langText = {
   ],
   _当前任务尚未完成2: [
     '当前任务尚未完成，请等待完成后再下载。',
-    '現在のタスクはまだ完了していません',
+    '現在のタスクはまだ完了していません、完了するまでお待ちください',
     'The current task has not yet been completed',
     '目前工作尚未完成，請等待完成後再下載。'
   ],
   _列表抓取完成开始获取作品页: [
     '当前列表中有{}张作品，开始获取作品信息',
-    '{}枚の作品があります。 作品情報の取得を開始します。',
+    '{} 枚の作品があります。 作品情報の取得を開始します。',
     'Now has {} works. Start getting the works for more information.',
     '目前清單中有{}張作品，開始取得作品資訊'
   ],
@@ -454,7 +454,7 @@ const langText = {
   ],
   _作品页状态码0: [
     '请求的url不可访问',
-    '要求されたURLにアクセスできません',
+    '要求された URL にアクセスできません',
     'The requested url is not accessible',
     '要求的url無法造訪'
   ],
@@ -466,7 +466,7 @@ const langText = {
   ],
   _作品页状态码403: [
     '无权访问请求的url 403',
-    'リクエストされたURLにアクセスできない 403',
+    'リクエストされた url にアクセスできない 403',
     'Have no access to the requested url 403',
     '無權造訪要求的url 403'
   ],
@@ -478,19 +478,19 @@ const langText = {
   ],
   _抓取图片网址的数量: [
     '已获取 {} 个图片网址',
-    '{} つの画像URLを取得',
+    '{} つの画像 url を取得',
     'Get {} image URLs',
     '已取得 {} 個圖片網址'
   ],
   _正在抓取: [
     '正在抓取，请等待……',
-    '取得中、しばらくお待ちください...',
+    'クロール中、しばらくお待ちください...',
     'Getting, please wait...',
     '正在擷取，請等待……'
   ],
   _获取全部书签作品: [
     '获取全部书签作品，时间可能比较长，请耐心等待。',
-    'ブックマークしたすべての作品を入手すると、時間がかかることがあります。お待ちください。',
+    'ブックマークしたすべての作品を取得すると、時間がかかることがあります。お待ちください。',
     'Get all bookmarked works, the time may be longer, please wait.',
     '取得全部書籤作品，時間可能比較長，請耐心等待。'
   ],
@@ -518,14 +518,14 @@ const langText = {
   ],
   _github: [
     'Github 页面，欢迎 star',
-    'Githubのページ、starをクリックしてください',
+    'Github のページ、star をクリックしてください',
     'Github page, if you like, please star it',
     'Github 頁面，歡迎 star'
   ],
   _wiki: ['使用手册', 'マニュアル', 'Wiki', 'Wiki'],
   _快捷键切换显示隐藏: [
     '使用 Alt + X，可以显示和隐藏下载面板',
-    'Alt + Xを使用してダウンロードパネルを表示および非表示にする',
+    'Alt + X を使用してダウンロードパネルを表示および非表示にする',
     'Use Alt + X to show and hide the download panel',
     '使用 Alt + X，可以顯示和隱藏下載面板'
   ],
@@ -543,25 +543,25 @@ const langText = {
   ],
   _设置文件夹名的提示: [
     `可以使用 '/' 建立文件夹<br>示例：{p_title}/{user}/{id}`,
-    `フォルダーは '/'で作成できます<br>例：{p_title}/{user}/{id}`,
+    `フォルダーは '/' で作成できます<br>例：{p_title}/{user}/{id}`,
     `You can create a directory with '/'<br>Example：{p_title}/{user}/{id}`,
     `可以使用 '/' 建立資料夾<br>範例：{p_title}/{user}/{id}`
   ],
   _添加字段名称: [
     '添加字段名称',
-    'フィールド名を追加',
+    'フィールドネームを追加',
     'Add field name',
     '加入欄位名稱'
   ],
   _添加字段名称提示: [
     '例如，在用户名前面添加“user_”标记',
-    'たとえば、ユーザー名の前に "user_" tagを追加します。',
+    'たとえば、ユーザー名の前に 「user_」 tag を追加します。',
     'For example, add the "user_" tag in front of the username',
     '例如，在使用者名稱前面加入“user_”標記'
   ],
   _查看标记的含义: [
     '查看标记的含义',
-    ' tagの意味を表示する',
+    ' tag の意味を表示する',
     'View the meaning of the tag',
     '檢視標記的意義'
   ],
@@ -583,7 +583,7 @@ const langText = {
   _命名标记7: ['宽度和高度', '幅と高さ', 'width and height', '寬度和高度'],
   _命名标记8: [
     'bookmark-count，作品的收藏数。把它放在最前面可以让文件按收藏数排序。',
-    'bookmark-count，作品のボックマークの数、前に追加することでボックマーク数に并べる。',
+    'bookmark-count，作品のボックマークの数、前に追加することでボックマーク数で并べることができます。',
     'bookmark-count, bookmarks number of works.',
     'bookmark-count，作品的收藏數。將它放在最前面可以讓檔案依收藏數排序。'
   ],
@@ -601,13 +601,13 @@ const langText = {
   ],
   _命名标记11: [
     '作品的 tag 列表，附带翻译后的 tag（如果有）',
-    '作品のtagリスト、翻訳付きtag(あれば)',
+    '作品の tag リスト、翻訳付き tag (あれば)',
     'The tags of the work, with the translated tag (if any)',
     '作品的 tag 清單，附帶翻譯後的 tag（若有的話）'
   ],
   _命名标记12: [
     '作品的创建日期，格式为 yyyy-MM-dd。如 2019-08-29',
-    '作品の作成日はyyyy-MM-ddの形式でした。 2019-08-29 など',
+    '作品の作成日は yyyy-MM-dd の形式でした。 2019-08-29 など',
     'The date the creation of the work was in the format yyyy-MM-dd. Such as 2019-08-29',
     '作品的建立日期，格式為 yyyy-MM-dd。如 2019-08-29'
   ],
@@ -625,7 +625,7 @@ const langText = {
   ],
   _命名标记提醒: [
     '您可以使用多个标记；建议在不同标记之间添加分割用的字符。示例：{id}-{userid}<br>一定要包含 {id} 或者 {id_num}。<br>* 在某些情况下，会有一些标记不可用。',
-    '複数の tagを使用することができ；異なる tag間に別の文字を追加することができます。例：{id}-{userid}<br>必ず{id}または{id_num}を含めてください。<br>* 場合によっては、一部の tagが利用できず。',
+    '複数のタグを使用することができます；異なるタグ間の分割のために文字を追加することをお勧めします。例：{id}-{userid}<br>必ず{id}または{id_num}を含めてください。<br>* 場合によっては、一部の tag が利用できず。',
     'You can use multiple tags, and you can add a separate character between different tags. Example: {id}-{userid}<br>Be sure to include {id} or {id_num}.<br>* In some cases, some tags will not be available.',
     '您可以使用多個標記；建議在不同標記之間加入分隔用的字元。範例：{id}-{userid}<br>一定要包含 {id} 或者 {id_num}。<br>* 在某些情況下，會有一些標記不可用。'
   ],
@@ -637,7 +637,7 @@ const langText = {
   ],
   _文件夹标记PUid: [
     '当前页面的画师id',
-    'アーティストID',
+    'アーティスト id',
     'Artist id of this page',
     '目前頁面的畫師id'
   ],
@@ -667,7 +667,7 @@ const langText = {
   ],
   _线程数字: [
     '可以输入 1-5 之间的数字，设置同时下载的数量',
-    '同時ダウンロード数を設定するには、1〜5 の数値を入力します',
+    '同時ダウンロード数を設定、1-5 の数値を入力してください',
     'You can type a number between 1-5 to set the number of concurrent downloads',
     '可以輸入 1-5 之間的數字，設定同時下載的數量'
   ],
@@ -679,7 +679,7 @@ const langText = {
     '暫停下載'
   ],
   _下载按钮3: ['停止下载', 'ダウンロードを停止', 'stop download', '停止下載'],
-  _下载按钮4: ['复制 url', 'URLをコピー', 'copy urls', '複製url'],
+  _下载按钮4: ['复制 url', 'URL をコピー', 'copy urls', '複製url'],
   _当前状态: ['当前状态 ', '現在の状態 ', 'Now state ', '目前狀態 '],
   _未开始下载: [
     '未开始下载',
@@ -771,19 +771,19 @@ const langText = {
   ],
   _相关作品大于0: [
     ' （下载相关作品必须大于 0）',
-    ' （ダウンロードする関連作品の数は0より大きくなければならない）',
+    ' 「ダウンロードする関連作品の数は0より大きくなければならない」',
     '  (Download related works must be greater than 0)',
     ' （下載相關作品必須大於 0）'
   ],
   _默认下载多页: [
     ', 如有多页，默认会下载全部。',
-    '複数のページがある場合、デフォルトですべてをダウンロードされます。',
+    '、複数のページがある場合、デフォルトですべてをダウンロードされます。',
     ', If there are multiple pages, the default will be downloaded.',
     ', 如有多頁，預設會下載全部。'
   ],
   _调整完毕: [
     '调整完毕，当前有{}个作品。',
-    '調整が完了し、今、{}の作品があります。',
+    '調整が完了し、今、{} の作品があります。',
     'The adjustment is complete and now has {} works.',
     '調整完畢，目前有{}個作品。'
   ],
@@ -849,7 +849,7 @@ const langText = {
   ],
   _抓取本页作品Title: [
     '抓取本页列表中的所有作品',
-    'このページをクロールする',
+    'このページの全ての作品をクロールする',
     'Crawl this page works',
     '擷取本頁清單中的所有作品'
   ],
@@ -897,7 +897,7 @@ const langText = {
   ],
   _数字提示1: [
     '-1, 或者大于 0',
-    '-1、または0より大きい',
+    '-1、または 0 より大きい',
     '-1, or greater than 0',
     '-1, 或是大於 0'
   ],
@@ -911,26 +911,26 @@ const langText = {
   _举报: ['举报', '報告', 'Report', '回報'],
   _输入id进行抓取: [
     '输入id进行抓取',
-    'IDを入力してダウンロードする',
+    'idを入力してダウンロードする',
     'Enter id to fetch',
     '輸入id進行擷取'
   ],
   _输入id进行抓取的提示文字: [
     '请输入作品id。如果有多个id，则以换行分割（即每行一个id）',
-    'イラストレーターIDを入力してください。 複数のidがある場合は、1行に1つのidを付けます。',
+    'イラストレーターIDを入力してください。 複数の id がある場合は、1 行に 1 つの id を付けます。',
     'Please type the illustration id. If there is more than one id, one id per line.',
     '請輸入作品id。如果有多個id，則以換行分隔（即每行一個id）'
   ],
   _开始抓取: ['开始抓取', 'クロールを開始する', 'Start crawling', '開始擷取'],
   _添加tag: [
     '给未分类作品添加 tag',
-    '未分類の作品にtagを追加',
+    '未分類の作品に tag を追加',
     'Add tag to unclassified work',
     '幫未分類的作品加入 tag'
   ],
   _id不合法: [
     'id不合法，操作取消。',
-    'idが不正な、操作はキャンセルされます。',
+    'id が不正な、操作はキャンセルされます。',
     'id is illegal, the operation is canceled.',
     'id不合法，動作取消。'
   ],
@@ -963,13 +963,13 @@ const langText = {
   _确定: ['确定', '確定', 'Ok', '確定'],
   _file404: [
     '404 错误：文件 {} 不存在。',
-    '404エラー：ファイル {} は存在しません。',
+    '404 エラー：ファイル {} は存在しません。',
     '404 error: File {} does not exist.',
     '404 錯誤：檔案 {} 不存在。'
   ],
   _文件下载失败: [
     '文件 {} 下载失败',
-    'ファイル {} のダウンロードに失敗しました',
+    'ファイル {} のダウンロードを失敗しました',
     'File {} download failed',
     '檔案 {} 下載失败'
   ],
@@ -998,10 +998,10 @@ const langText = {
     'When downloading quickly, if there is only one picture, a directory is also created',
     '快速下載時，若只有一張圖片，也會建立資料夾'
   ],
-  _设置id范围: ['设置 id 范围', 'ID範囲を設定', 'Set id range', '設定 id 範圍'],
+  _设置id范围: ['设置 id 范围', 'id 範囲を設定', 'Set id range', '設定 id 範圍'],
   _设置id范围提示: [
     '您可以输入一个作品 id，抓取比它新或者比它旧的作品',
-    '1つの作品IDを入力することで、それより新しいあるいは古い作品をクロールことができます',
+    '1 つの作品 id を入力することで、それより新しいあるいは古い作品をクロールことができます',
     'You can enter a work id and crawl works that are newer or older than it',
     '您可以輸入一個作品 id，擷取比它新或者比它舊的作品。'
   ],
@@ -1029,7 +1029,7 @@ const langText = {
   _开始筛选: ['开始筛选', 'スクリーニング開始', 'Start screening', '開始篩選'],
   _开始筛选Title: [
     '按照设置来筛选当前 tag 里的作品。',
-    '現在のtagにある作品を設定によってスクリーニングする',
+    '現在の tag にある作品を設定によってスクリーニングする',
     'Screen the works in the current tag.',
     '按照設定來篩選當前 tag 裡的作品。'
   ],
@@ -1077,7 +1077,7 @@ const langText = {
   ],
   _xzNew410: [
     '当你下载多图作品时，下载器可以使用作品 id 自动创建一个目录，保存里面的图片。',
-    'マルチイメージをダウンロードする時、自動的に作品idを使ってフォルダを作成し、中のイラストを保存することができます。',
+    'マルチイメージをダウンロードする時、自動的に作品 id を使ってフォルダを作成し、中のイラストを保存することができます。',
     'When you download a multi-image work, the downloader can automatically create a directory with the work id and save the images inside.',
     '當你下載多圖作品時，下載器可以使用作品 id 自動創建一個目錄，保存裏面的圖片。'
   ]
