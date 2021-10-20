45ab4acb16d9c6f391decdd2235bd125a8d3d08b
xuejianxianzun <1247756698@qq.com>
2021-10-20 14:02:54 +0800

    format

--- src/ts/SelectWork.ts
+++ src/ts/SelectWork.ts
@@ -237,13 +237,17 @@ class SelectWork {
     // 在已关注用户的最新作品-插画里
     if (pageType.type === pageType.list.NewArtworkBookmark) {
       const newPage = !document.querySelector('h1')
-      const selector = newPage ? 'li[size="1"]': '#js-mount-point-latest-following>div>div'
+      const selector = newPage
+        ? 'li[size="1"]'
+        : '#js-mount-point-latest-following>div>div'
       works = document.querySelectorAll(selector)
     }
     // 在已关注用户的最新作品-小说里
     if (pageType.type === pageType.list.NewNovelBookmark) {
       const newPage = !document.querySelector('h1')
-      const selector = newPage ? 'li[size="1"]': '#js-mount-point-latest-following>div>div'
+      const selector = newPage
+        ? 'li[size="1"]'
+        : '#js-mount-point-latest-following>div>div'
       works = document.querySelectorAll(selector)
     }
     if (works.length === 0) {
