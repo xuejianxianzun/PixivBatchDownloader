22443df09213fd205001443bd40cad38e50ffaab
xuejianxianzun <1247756698@qq.com>
2022-06-23 20:36:29 +0800

    修复一处页面类型判断错误的情况

--- dist/js/content.js
+++ dist/js/content.js
@@ -5633,7 +5633,7 @@ class PageType {
             ['/', '/manga', '/novel/', '/en/'].includes(pathname)) {
             return PageName.Home;
         }
-        else if (/\/artworks\/\d{1,10}/.test(url)) {
+        else if (location.pathname.startsWith('/artworks') && /\/artworks\/\d{1,10}/.test(url)) {
             return PageName.Artwork;
         }
         else if (/\/users\/\d+/.test(url) && !url.includes('/bookmarks')) {
