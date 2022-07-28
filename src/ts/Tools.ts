4cce9d11a2b7ee9ab58f3383fae070ad4144d0ea
xuejianxianzun <1247756698@qq.com>
2022-07-29 00:33:56 +0800

    :ambulance:修复了极少数特殊的动图会转换失败的问题

--- src/ts/Tools.ts
+++ src/ts/Tools.ts
@@ -10,10 +10,10 @@ type artworkDataTagsItem = {
   userId: string
   romaji: string
   translation?:
-    | {
-        en: string
-      }
-    | undefined
+  | {
+    en: string
+  }
+  | undefined
   userName: string
 }
 
@@ -210,7 +210,7 @@ class Tools {
     if (document.body) {
       document.body.insertAdjacentElement('afterbegin', el)
     } else {
-      ;(
+      ; (
         document.querySelector('.newindex-inner')! ||
         document.querySelector('.layout-body')!
       ).insertAdjacentElement('beforebegin', el)
@@ -441,7 +441,6 @@ class Tools {
    * @returns number[] 返回一个索引列表的数组
    *
    */
-  // zip 文件结尾有 000000.jpgPK 这样的标记，需要排除，因为这是 zip 的文件目录，不是图片
   static getJPGContentIndex(
     zipFile: ArrayBuffer,
     existingIndexList?: number[]
@@ -455,7 +454,6 @@ class Tools {
     let offset = 0
     // 循环的次数
     let loopTimes = 0
-
     // console.time('getJPGContentIndex')
     while (true) {
       // 如果当前偏移量的后面有已经查找到的索引，就不必重复查找了
@@ -476,16 +474,24 @@ class Tools {
         data = new Uint8Array(zipFile, offset)
       }
 
-      // 查找以 0 开头，长度为 10，以 jpg 结束的值的索引
+      // 查找以 jpg 文件名的标记，如 000000.jpg
+      // 其实文件内容里也可能会出现符合条件的数据，只是概率很小
+      // 有些文件名后面有 PK 标记，如 000000.jpgPK，需要排除，因为这是 zip 的文件目录，不是图片
       const index = data.findIndex((val, index2, array) => {
-        // 0 j p g P
+        // 0 0 0 . j p g P
         if (
           val === 48 &&
+          array[index2 + 1] === 48 &&
+          array[index2 + 2] === 48 &&
+          array[index2 + 6] === 46 &&
           array[index2 + 7] === 106 &&
           array[index2 + 8] === 112 &&
           array[index2 + 9] === 103 &&
           array[index2 + 10] !== 80
         ) {
+          // const enc = new TextDecoder('utf-8')
+          // const buff = data.slice(index2, index2 + 10)
+          // console.log(enc.decode(buff))
           return true
         }
         return false
@@ -497,10 +503,10 @@ class Tools {
         offset = fileContentStart
         ++loopTimes
       } else {
+        // console.timeEnd('getJPGContentIndex')
         return indexList
       }
     }
-    // console.timeEnd('getJPGContentIndex')
   }
 
   /**从 zip 压缩包里提取出图像数据，转换成 img 标签列表 */
