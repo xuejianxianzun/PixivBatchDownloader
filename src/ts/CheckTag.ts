ef3f732bc09855d71aae2d888b548f91958bac61
xuejianxianzun <1247756698@qq.com>
2023-05-28 00:19:38 +0800

    暂存

---
+++ src/ts/CheckTag.ts
@@ -0,0 +1,24 @@
+// 当鼠标放在作品页面内的 tag 上时，检查这个 tag 是否存在于下载器的某些 tag 过滤设置里，并显示对应操作和提示
+class CheckTag {
+
+  /**监听页面变化，寻找 tag 元素 */
+  private observe(){}
+
+  /**为 tag 元素绑定事件，鼠标放上去再检测 */
+  private bindEvent(){}
+
+  /**检查下载器的 tag 过滤设置，决定对这个 tag 应该显示什么提示和操作 */
+  private check(){}
+
+  /**把这个 tag 添加到 tag 屏蔽列表 */
+  private addToExcludeList(){}
+
+  /**这个 tag 从屏蔽列表中移除*/
+  private removeFormExcludeList(){}
+
+  /**把这个 tag 从 tag 重命名列表中移除 */
+  private removeFormTagRename(){}
+
+}
+
+new CheckTag()
